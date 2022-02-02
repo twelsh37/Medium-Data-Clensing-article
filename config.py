6b7cba9abae63e7156c4cccf220da43c30051f14
twelsh37 <4956770+twelsh37@users.noreply.github.com>
2022-02-02 22:20:43 +0000

    ignore config.py

--- config.py
+++
@@ -1,6 +0,0 @@
-# Config file to hold our mapbox token
-# We set this up here so we dont distribute our mapbox token along with our code.
-# we add this to the.gitignore file
-
-# Set our mapbox token that we get from our mapbox account
-token = 'pk.eyJ1IjoidHdlbHNoMzciLCJhIjoiY2tqMHF0Zm1iMnVoazJybjQwZmFiNjJuciJ9.RSEYFXvHhpjoxrtkbLIuWg'
